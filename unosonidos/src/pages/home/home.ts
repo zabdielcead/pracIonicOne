5e1450f4471574c8652dba3d18f7ac0c9c96f291
zabdielcead <zabdiel.cead@gmail.com>
2018-11-23 15:52:01 -0600

    se sube la app con funcionalidad

--- unosonidos/src/pages/home/home.ts
+++ unosonidos/src/pages/home/home.ts
@@ -1,14 +1,53 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { ANIMALES } from '../../data/data.animales';
+import { Animal } from '../../interfaces/animal.interface';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  animalesx:Animal[] = [];
+  audio = new Audio();
+  audioTiempo:any;
 
   constructor(public navCtrl: NavController) {
+    this.animalesx = ANIMALES.slice(0);
+  }
+  
+  reproducir(animal:Animal){
+    console.log(animal);
+    this.pausar_audio(animal);
 
+    if(animal.reproduciendo){
+      animal.reproduciendo = false;
+      return;
+    }
+
+    console.log("reproduciendo..");
+
+    this.audio.src  = animal.audio;
+    
+    this.audio.load();
+    this.audio.play();
+
+    animal.reproduciendo = true;
+    console.log("FIN reproduciendo..");
+    this.audioTiempo = setTimeout( ()=>  animal.reproduciendo = false, animal.duracion * 1000 );
+
+    
   }
 
+
+  private pausar_audio(animalSel:Animal){
+    clearTimeout(this.audioTiempo);
+    this.audio.pause();
+    this.audio.currentTime = 0;
+    for (let animal of this.animalesx){
+      if(animal.nombre != animalSel.nombre){
+        animal.reproduciendo = false;
+      }
+    }
+
+  }
 }
